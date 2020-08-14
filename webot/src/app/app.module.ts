ee958b12265061da9fa50812767fb7e740e3c9ea
Alexander Eberle <alexander171294@gmail.com>
2020-08-14 11:54:42 -0300

    all pages

--- webot/src/app/app.module.ts
+++ webot/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComandosComponent } from './pages/comandos/comandos.component';
+import { VariablesComponent } from './pages/variables/variables.component';
+import { ConfiguracionComponent } from './pages/configuracion/configuracion.component';
+import { LogsComponent } from './pages/logs/logs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComandosComponent
+    ComandosComponent,
+    VariablesComponent,
+    ConfiguracionComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,

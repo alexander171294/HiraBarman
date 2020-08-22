18c9ad7604a801c71eacb7a680ffd77ca20332db
Alexander Eberle <alexander171294@gmail.com>
2020-08-22 15:29:08 -0300

    configuration service.

---
+++ webot/src/app/pages/configuracion/configuraciones.service.ts
@@ -0,0 +1,40 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfiguracionesService {
+
+  constructor(private httpC: HttpClient) { }
+
+  public getNick(): Observable<string> {
+    return this.httpC.get(environment.botApi + '/settings/nick', {responseType: 'text'});
+  }
+
+  public setNick(nick: string): Observable<string> {
+    return this.httpC.post(environment.botApi + '/settings/nick?nick=' + nick, {}, {responseType: 'text'});
+  }
+
+  public getChannels(): Observable<string[]> {
+    return this.httpC.get<string[]>(environment.botApi + '/settings/channels');
+  }
+
+  public joinChannel(channel: string): Observable<string[]> {
+    return this.httpC.post<string[]>(environment.botApi + '/settings/channels?chan=' + channel, {});
+  }
+
+  public leaveChannel(channel: string): Observable<string[]> {
+    return this.httpC.delete<string[]>(environment.botApi + '/settings/channels?chan=' + channel);
+  }
+
+  public getOwners(): Observable<string[]> {
+    return this.httpC.get<string[]>(environment.botApi + '/settings/owners');
+  }
+
+  public setOwners(owners: string[]): Observable<string[]> {
+    return this.httpC.put<string[]>(environment.botApi + '/settings/owners', owners);
+  }
+}

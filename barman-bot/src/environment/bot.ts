429a1fcc9f99abcb5ff88396bf216867ed525789
Alexander Eberle <alexander171294@gmail.com>
2021-04-22 15:33:42 -0300

    fixing dockerfile

--- barman-bot/src/environment/bot.ts
+++ barman-bot/src/environment/bot.ts
@@ -1,12 +1,11 @@
 export const botCFG = {
 	// "channels": ["#harkolandia", "#caballeros"],
-	"channels": [
-		// "#harkolandia",
+	"channels": process.env.BOT_CHANNELS ? : process.env.BOT_CHANNELS.split(',') [
 		"#LaTaberna"
 	],
-	"server": "irc.hirana.net",
-	"botName": "Barman",
-	"owners": [
+	"server": process.env.BOT_SERVER ? process.env.BOT_SERVER : 'irc.hirana.net',
+	"botName": process.env.BOT_NAME ? process.env.BOT_NAME : 'BarmanBot',
+	"owners": process.env.BOT_OWNERS ? process.env.BOT_OWNERS.split(',') : [
 		'alex',
 		'maguito',
 		'gabriela-'
